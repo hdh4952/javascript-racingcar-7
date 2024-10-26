b42b39b79fc223984cf29b3ee53bcbbaee8d6181
DaeHee Han <gkseogml4959@gmail.com>
2024-10-26 16:14:56 +0900

    refactor: class Output, Car, Race

--- src/App.js
+++ src/App.js
@@ -43,59 +43,68 @@ const Input = (() => {
   };
 })();
 
-
-class App {
-  async run() {
-    const cars = await this.getCars();
-    const moveCount = await this.getMoveCount();
-    const winners = this.raceCarsForCount(cars, moveCount);
-    this.outputWinners(winners);
+const Output = (() => {
+  class Output {
+    static print(object) {
+      MissionUtils.Console.print(object);
+    }
   }
 
-  async getCars() {
-    const input = await Input.readInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const carNames = input.parseCarNames();
-    const cars = this.createCarsWithNames(carNames);
-    return cars;
+  return {
+    print: Output.print
   }
-  
-  createCarsWithNames(names) {
-    return names.map(name => ({name, distance: 0}));
+})();
+
+class Car {
+  #name;
+  #distance;
+
+  constructor(name) {
+    this.#name = name;
+    this.#distance = 0;
   }
 
-  async getMoveCount() {
-    const input = await Input.readInput('시도할 횟수는 몇 회인가요?\n');
-    const moveCount = input.parseMoveCount();
-    return moveCount;
+  moveFoward() {
+    this.#distance++;
   }
 
-  raceCarsForCount(cars, count) {
+  toString() {
+    return `${this.#name} : ${'-'.repeat(this.#distance)}`;
+  }
+
+  compareTo(other) {
+    return other.#distance - this.#distance; 
+  }
+
+  get name() {
+    return this.#name;
+  }
+}
+
+class Race {
+  #cars;
+  #log;
+
+  constructor(cars) {
+    this.#cars = cars;
+    this.#log = '';
+  }
+
+  start(count) {
     if (count <= 0) {
-      return this.winnersForCars(cars);
+      return;
     }
 
-    const carsAfterRace = this.raceCars(cars);
-    this.outputCars(carsAfterRace);
-    return this.raceCarsForCount(carsAfterRace, count - 1);
+    this.#cars = this.#cars.map((car) => this.raceWithLog(car));
+    this.start(count - 1);
   }
 
-  raceCars(cars) {
-    const carsAfterRace = cars.map((car) => this.raceCar(car));
-    return carsAfterRace;
-  }
-
-  outputCars(cars) {
-    cars.forEach(car => {
-      MissionUtils.Console.print(`${car.name} : ${'-'.repeat(car.distance)}`)
-    });
-  }
-
-  raceCar(car) {
+  raceWithLog(car) {
     if (this.isCarMovable()) {
-      const movedCar = this.moveFoward(car);
-      return movedCar;
+      car.moveFoward();
     }
-  
+
+    this.#log += car;
     return car;
   }
 
@@ -106,30 +115,54 @@ class App {
     return false;
   }
 
-  moveFoward(car) {
-    return {...car, distance: car.distance + 1};
+  get result() {
+    const winners = this.#cars.slice(1).reduce((winners, car) => {
+      const cmp = winners[0].compareTo(car);
+      if (cmp > 0) {
+        return [car];
+      }
+
+      if (cmp === 0) {
+        winners.push(car);
+      }
+
+      return winners;
+    }, [this.#cars[0]]);
+
+    this.#log += '최종 우승자 : ' + winners.map(winner => winner.name).join(', ');
+    return this.#log;
+  }
+}
+
+class App {
+  async run() {
+    const cars = await this.getCars();
+    const moveCount = await this.getMoveCount();
+    const result = this.raceCarsForCount(cars, moveCount);
+    Output.print(result);
   }
 
-  winnersForCars(cars) {
-    const { winners } = cars.reduce((acc, cur) => {
-      if (cur.distance >= acc.maxDistance) {
-        acc.winners.push(cur.name);
-        acc.maxDistance = cur.distance;
-        return acc;
-      } 
-
-      return {
-        winners: [cur.name], 
-        maxDistance: cur.distance
-      };
-    }, {winners: [], maxDistance: 0}); 
-
-    return winners;
+  async getCars() {
+    const input = await Input.readInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = input.parseCarNames();
+    const cars = this.createCars(carNames);
+    return cars;
+  }
+  
+  createCars(names) {
+    return names.map(name => new Car(name));
   }
 
-  outputWinners(winners) {
-    const result = '최종 우승자 : ' + winners.join(', ');
-    MissionUtils.Console.print(result);
+  async getMoveCount() {
+    const input = await Input.readInput('시도할 횟수는 몇 회인가요?\n');
+    const moveCount = input.parseMoveCount();
+    return moveCount;
+  }
+
+  raceCarsForCount(cars, count) {
+    const race = new Race(cars);
+    race.start(count);
+    return race.result;
   }
 }
 
